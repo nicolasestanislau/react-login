3e57a8cd8ac33834a3431507f8c1b8367df57d8e
Nicolas Estanislau <nicolas_estanislau@hotmail.com>
2021-11-12 02:04:01 -0300

    first commit

--- src/components/User/Login/Login.jsx
+++ src/components/User/Login/Login.jsx
@@ -1,20 +1,58 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
+import { useHistory } from 'react-router-dom';
+import StoreContext from 'components/Store/Context';
 import UIButton from 'components/UI/Button/Button';
 
 import './Login.css';
 
+function initialState() {
+  return { user: '', password: '' }
+}
+
+function login({ user, password }) {
+  if (user === 'admin' && password === 'admin') {
+    return { token: '1234' }
+  }
+  return { error: 'Usuario ou senha invalido' }
+
+}
 const UserLogin = () => {
+  const [values, setValues] = useState(initialState);
+  const { setToken } = useContext(StoreContext);
+  const history = useHistory();
+
+  function onChange(event) {
+    const { value, name } = event.target;
+    setValues({
+      ...values,
+      [name]: value
+    })
+
+  }
+
+  function onSubmit(event) {
+    event.preventDefault();
+
+    const { token } = login(values);
+
+    if (token) {
+      setToken(token)
+      return history.push('/')
+    }
+
+    setValues(initialState)
+  }
   return (
     <div className="user-login">
       <h1 className="user-login__title">Acessar o Sistema</h1>
-      <form autoComplete="nope">
+      <form autoComplete="nope" onSubmit={onSubmit}>
         <div className="user-login__form-control">
           <label htmlFor="email">E-mail</label>
-          <input id="email" type="text" name="email" autoComplete="off" />
+          <input id="user" type="text" name="user" autoComplete="off" onChange={onChange} value={values.user} />
         </div>
         <div className="user-login__form-control">
           <label htmlFor="password">Senha</label>
-          <input id="password" type="password" name="password" />
+          <input id="password" type="password" name="password" onChange={onChange} value={values.password} />
         </div>
         <UIButton
           type="submit"
